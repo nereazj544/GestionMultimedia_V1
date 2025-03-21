d5aa450dda6498a2c111efe22f80cd353857c2e6
nereazj544 <nzjvirtual@gmail.com>
2025-03-21 19:17:20 +0100

    Agregar componentes Home y Error con sus respectivas rutas y estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: ErrorComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
