310512a8c3a5f15a9f96f345ac36917e8fc6fa33
nereazj544 <nzjvirtual@gmail.com>
2025-03-22 16:08:03 +0100

    Actualizar el componente Home con nuevo contenido y estilos; agregar fuente Roboto y estilos para botones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy{
+
+  palabras: string[] = ['Libros', 'Videojuegos', 'Peliculas', 'Series']
+  palabraAc: string = ' ';
+  i: number = 0;
+
+  private intevaloId: any;
+
+  ngOnInit(): void {
+      this.PalabrasCambio();
+  }
+
+  ngOnDestroy(){
+    if (this.intevaloId) {
+      clearInterval(this.intevaloId);
+      this.intevaloId = null;
+    }
+  }
+
+  PalabrasCambio(){
+    if (this.intevaloId) {
+      clearInterval(this.intevaloId)
+    }
+
+    this.palabraAc = this.palabras[0];
+    this.intevaloId = setInterval(() => {
+      this.i = (this.i + 1)  % this.palabras.length;
+      this.palabraAc = this.palabras[this.i]
+    }, 3000)
+  }
+
 
 }
